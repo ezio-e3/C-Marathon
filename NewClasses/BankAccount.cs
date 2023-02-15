cc52aab9ebae1f6b4969a7a46515c14db1101ad7
Edinam <edin22.ey@gmail.com>
2023-02-15 06:17:33 +0000

    added methods for withdrawal and deposit

--- NewClasses/BankAccount.cs
+++ NewClasses/BankAccount.cs
@@ -1,22 +1,50 @@
 using System;
 using System.Collections.Generic;
 using NewClasses;
-namespace NewClasses { 
+namespace NewClasses {
 
-public class BankAccount
-{
-    private static int accountNumberSeed = 123456789;
-    private List<Transactions> allTransactions = new List<Transactions>();
-    public string Number { get; }
-    public string Owner { get; set; }
-    public decimal Balance { get; }
+    public class BankAccount
+    {
+        private static int accountNumberSeed = 123456789;
+        private List<Transactions> allTransactions = new List<Transactions>();
+        public string Number { get; }
+        public string Owner { get; set; }
+        private decimal _balance;
+        public decimal Balance {
+            get
+            {
+                decimal _balance = 0;
+                foreach (var item in allTransactions)
+                {
+                    _balance += item.Amount;
+                }
+                return _balance;
+            }
+            set {
+                _balance = value;
+            }
+        }
+            
 
     public void MakeDeposit(decimal amount, DateTime date, string note)
     {
+            if (amount <= 0) {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+            var deposit = new Transactions(amount, date, note);
+            allTransactions.Add(deposit);
     }
 
     public void MakeWithdrawal(decimal amount, DateTime date, string note)
     {
+            if (amount <= 0) {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+            if (Balance - amount < 0) {
+                throw new InvalidOperationException("Not sufficient funds for the withdrawal");
+            }
+            var withdrawal = new Transactions(-amount, date, note);
+            allTransactions.Add(withdrawal);
     }
 
     public BankAccount(string name, decimal initialBalance)
@@ -25,7 +53,7 @@ public class BankAccount
         Balance = initialBalance;
         this.Number = accountNumberSeed.ToString();
             accountNumberSeed++;
-
+            MakeDeposit(initialBalance, DateTime.Now, "Initial Balance");
     }
 }
 }
--- NewClasses/Program.cs
+++ NewClasses/Program.cs
@@ -6,10 +6,15 @@ namespace NewClasses
     {
         static void Main(string[] args)
         {
-            var account = new BankAccount("<name>", 1000);
-            var account2 = new BankAccount("<name>", 20000);
+            var account = new BankAccount("Yawenam Ahadzie", 40000);
             Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+            var account2 = new BankAccount("Edinam Ahadzie", 800000);
             Console.WriteLine($"Account {account2.Number} was created for {account2.Owner} with {account2.Balance} initial balance.");
+
+            account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+            Console.WriteLine(account.Balance);
+            account.MakeDeposit(100, DateTime.Now, "Friend pay me back");
+            Console.WriteLine(account.Balance);
         }
     }
 }
